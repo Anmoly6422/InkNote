58b65951f46fca93fff9d6409e5daee7b3850827
Anmoly6422 <231046@kit.ac.in>
2026-05-28 10:51:52 +0530

    InkNote

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:process.env.VITE_BASE_PATH || "https://github.com/Anmoly6422/InkNote",
+  base:process.env.VITE_BASE_PATH || "InkNote",
 })
